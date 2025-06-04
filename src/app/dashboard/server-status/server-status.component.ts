d4b0bbcdf76eceb447250f3423234bf720c38d71
Ibrahim-Hassan74 <ih3046376@gmail.com>
2025-06-04 21:54:09 +0300

    implement OnInit interface to avoid errors

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,9 +7,8 @@ import { Component } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css',
 })
-export class ServerStatusComponent {
+export class ServerStatusComponent implements OnInit {
   currentStatus: string = 'online';
-  constructor() {}
   ngOnInit() {
     setInterval(() => {
       const statuses = ['online', 'offline', 'unknown'];
