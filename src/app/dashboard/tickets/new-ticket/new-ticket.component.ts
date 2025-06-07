6e32234dc99a49952e0e9b776adb6c8a81816363
Ibrahim-Hassan74 <ih3046376@gmail.com>
2025-06-07 20:59:31 +0300

    use viewChild signal instead ViewChild decorator

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, viewChild, ViewChild } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
 import { FormsModule } from '@angular/forms';
@@ -11,7 +11,11 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './new-ticket.component.css',
 })
 export class NewTicketComponent {
+  // @ViewChild('form') form?: ElementRef<HTMLFormElement>;
+  private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
   onSubmit(title: string, ticketText: string) {
     console.log(title, ticketText);
+    this.form().nativeElement.reset();
+    // console.dir(this.form());
   }
 }
